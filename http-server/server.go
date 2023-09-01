b540a11d21831a89f07af87846e138232fab1631
Mariam Gustavo <yegetamariam@gmail.com>
2023-09-01 09:59:17 +0300

    added league of players and json

--- http-server/server.go
+++ http-server/server.go
@@ -1,53 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
 )
 
-type PlayerStore interface { // we need some kind of storage system after getting the player scores
+// PlayerStore stores score information about players.
+type PlayerStore interface {
 	GetPlayerScore(name string) int
 	RecordWin(name string)
-}
-type PlayerServer struct { // making PlayerServer a struct to implement handler interface
-	store PlayerStore // interface polymorphism or duck typing PlayerServer can now get access to GetPlayerScore an RecordWin
-}
-type StubPlayerStore struct { // fake storage
-	scores   map[string]int
-	winCalls []string
+	GetLeague() []Player
 }
 
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	score := s.scores[name]
-	return score
-}
-func (s *StubPlayerStore) RecordWin(name string) {
-	s.winCalls = append(s.winCalls, name)
+// Player stores a name with a number of wins.
+type Player struct {
+	Name string
+	Wins int
 }
 
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	//handling GET OR POST based on http.Method
+// PlayerServer is a HTTP interface for player information.
+type PlayerServer struct {
+	store PlayerStore
+	http.Handler
+}
+
+const jsonContentType = "application/json"
+
+// NewPlayerServer creates a PlayerServer with routing configured.
+func NewPlayerServer(store PlayerStore) *PlayerServer {
+	p := new(PlayerServer)
+
+	p.store = store
+
+	router := http.NewServeMux()
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	router.Handle("/", http.HandlerFunc(p.rootHandler))
+
+	p.Handler = router
+
+	return p
+}
+func (p *PlayerServer) rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
+func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", jsonContentType)
+	json.NewEncoder(w).Encode(p.store.GetLeague())
+}
+
+func (p *PlayerServer) playersHandler(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
 	switch r.Method {
 	case http.MethodPost:
-		p.processWin(w, r)
+		p.processWin(w, player)
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, player)
 	}
 }
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-	//Handling GET
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	score := p.store.GetPlayerScore(player) // as you can see here PlayerServer can access the GetPlayerScore method
-	if score == 0 {                         // handling 404 StatusNotFoundCase
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
-	fmt.Fprint(w, score)
 
+	fmt.Fprint(w, score)
 }
-func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
-	//HandlingPOST
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
 	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
